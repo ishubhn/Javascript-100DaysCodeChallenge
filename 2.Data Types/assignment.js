38f05bb024db19fc58901f48d77eab258f2feeb2
ishubhn <imshubhn@gmail.com>
2020-12-07 21:20:28 +0530

    Data types

---
+++ 2.Data Types/assignment.js
@@ -0,0 +1,17 @@
+let country = 'India';      //string
+
+let continent = 'Asia';     //string
+
+let population = 130000000; //number
+
+// console.log('My country is: '+ country + ', it is in ' + continent + '. And we have ' + population + ' peoples');
+
+let isIsland = 'false';  //boolean
+
+let language;       //unidentified
+
+console.log('Country is: ' + typeof(country));
+console.log('continent ' + typeof(continent));
+console.log('population ' + typeof(population));
+console.log('isIsland ' + typeof(isIsland));
+console.log('language ' + typeof(language));
