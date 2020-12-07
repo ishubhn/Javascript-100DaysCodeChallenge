9ecb405d0fd5eded7c1c4a81e8b6045dfcf522ff
ishubhn <imshubhn@gmail.com>
2020-12-07 21:08:33 +0530

    clear

---
+++ Day1/assignment.js
@@ -0,0 +1,7 @@
+let country = 'India';
+
+let continent = 'Asia';
+
+let population = 130000000;
+
+console.log('My country is: '+ country + ', it is in ' + continent + '. And we have ' + population + ' peoples');
