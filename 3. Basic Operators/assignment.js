e7fbbda77a5301cb83b3c68b50ce58435a72e709
ishubhn <imshubhn@gmail.com>
2020-12-07 21:26:44 +0530

    let, const and var

---
+++ 3. Basic Operators/assignment.js
@@ -0,0 +1,17 @@
+const country = 'India';      //string
+
+const continent = 'Asia';     //string
+
+let population = 130000000; //number
+
+// console.log('My country is: '+ country + ', it is in ' + continent + '. And we have ' + population + ' peoples');
+
+const isIsland = 'false';  //boolean
+// country = 'USA';             #Can't change const values - throws TypeError"
+let language = "Marathi";       //unidentified
+
+console.log('Country is: ' + typeof(country));
+console.log('continent ' + typeof(continent));
+console.log('population ' + typeof(population));
+console.log('isIsland ' + typeof(isIsland));
+console.log('language ' + typeof(language));
