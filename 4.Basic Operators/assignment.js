c689a87d937ce81277ec27385ebe6c748d5c0a32
ishubhn <imshubhn@gmail.com>
2020-12-10 20:00:57 +0530

    Basic Operators

---
+++ 4.Basic Operators/assignment.js
@@ -0,0 +1,19 @@
+const country = 'India';      //string
+
+const continent = 'Asia';     //string
+
+let population = 130000000; //number
+
+// console.log('My country is: '+ country + ', it is in ' + continent + '. And we have ' + population + ' peoples');
+
+const isIsland = 'false';  //boolean
+// country = 'USA';             #Can't change const values - throws TypeError"
+let language = "Marathi";       //unidentified
+
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6000000);
+console.log(33000000 > population);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
